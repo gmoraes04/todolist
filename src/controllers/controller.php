2b2a1b2458bf1ab9664b3f6e770b01751f2ab315
Gabriel Moraes <gabrielmoraessilva0403@gmail.com>
2025-04-10 21:43:42 -0300

    criando o controller

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -1,9 +1,23 @@
 <?php
 // Adicione aqui os includes e/ou requires
-
+require_once __DIR__."/../models/models.php"
 // Função que manipula as requsições do servidor
 function handleRequest(){
     //Seu código aqui
+    if(isset($_POST['action'])){
+        switch($_POST['action']){
+            case 'add';
+            if(!empty($_POST['description'])){
+                addNewTask($_POST['description']);
+            }
+            break;
+        case 'toggle';
+            if(!empty($_POST['id'])){
+                toggleTask($_POST['id']);
+            }
+            break;    
+        }
+    }
 }
 
 // Função que exibe as tasks na view
